d482396bcb48ca20c354474a47e85b2fb491cefe
AdmiringWorm <kim.nordmo@gmail.com>
2022-09-03 13:28:02 +0200

    test: add tests for configuration class

---
+++ src/VideoConverter.Core.Tests/Models/ConfigurationTests.cs
@@ -0,0 +1,64 @@
+﻿namespace VideoConverter.Core.Tests.Models
+{
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+
+	using NUnit.Framework;
+
+	using VideoConverter.Core.Models;
+
+	using static VerifyNUnit.Verifier;
+
+	public class ConfigurationTests
+	{
+		[Test]
+		public async Task CanResetEntireClass()
+		{
+			var config = new Configuration
+			{
+				AudioCodec = "testing",
+				ExtraEncodingParameters = "Some Extra",
+				Fansubbers = new List<FansubberConfiguration>
+				{
+					new FansubberConfiguration
+					{
+						IgnoreOnDuplicates = true,
+						Name = "Some Fansubber"
+					}
+				},
+				FileType = "MPEG4",
+				IncludeFansubber = false,
+				MapperDatabase = "C:\testing",
+				Prefixes = new List<PrefixConfiguration>
+				{
+					new PrefixConfiguration
+					{
+						Path = "Some-Path",
+						Prefix = "prefix"
+					}
+				},
+				SubtitleCodec = "test2",
+				VideoCodec = "test3",
+				WorkDirectory = "C:\\worlk-dir"
+			};
+
+			config.Reset();
+
+			await Verify(config);
+		}
+
+		[Test]
+		public async Task NewInstanceAndResettingClassShouldBeTheSame()
+		{
+			var newConfig = new Configuration();
+			var resetConfig = new Configuration();
+			resetConfig.Reset();
+
+			await Verify(new
+			{
+				newConfig,
+				resetConfig
+			});
+		}
+	}
+}
