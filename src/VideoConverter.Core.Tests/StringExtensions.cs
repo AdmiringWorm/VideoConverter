59e781842ab9aca1ba0d9da48698865f00bb52e4
AdmiringWorm <kim.nordmo@gmail.com>
2022-11-29 09:12:00 +0100

    test: add unit and integration tests for checksum provider

---
+++ src/VideoConverter.Core.Tests/StringExtensions.cs
@@ -0,0 +1,18 @@
+namespace VideoConverter.Core.Tests
+{
+	using System.IO;
+	using System.Text;
+
+	public static class StringExtensions
+	{
+		public static MemoryStream ToStream(this string input)
+		{
+			var stream = new MemoryStream(Encoding.UTF8.GetBytes(input))
+			{
+				Position = 0
+			};
+
+			return stream;
+		}
+	}
+}
---
+++ src/VideoConverter.IntegrationTests/Services/ChecksumHashProviderTests.cs
@@ -0,0 +1,113 @@
+namespace VideoConverter.IntegrationTests.Services
+{
+	using System;
+	using System.IO;
+
+	using FluentAssertions;
+
+	using NUnit.Framework;
+
+	using VideoConverter.Core.Services;
+
+	[Parallelizable(ParallelScope.Children)]
+	public class ChecksumHashProviderTests
+	{
+		private static readonly IHashProvider provider = new ChecksumHashProvider();
+
+		[Test]
+		public void Should_Calculate_Expected_Checksum_From_Blank_File_Path()
+		{
+			const string expected = "DA39A3EE5E6B4B0D3255BFEF95601890AFD80709";
+			var testFile = Path.GetTempFileName();
+
+			try
+			{
+				var actual = provider.ComputeHash(testFile);
+				actual.Should().Be(expected);
+			}
+			finally
+			{
+				File.Delete(testFile);
+			}
+		}
+
+		[Test]
+		public void Should_Calculate_Expected_Checksum_From_File_Path()
+		{
+			const string expected = "FB5A1DA0421EDB02BA9D73C1A711445691046FDD";
+			var testFile = CreateTestFile(52428800); // ~50 MB
+
+			try
+			{
+				var actual = provider.ComputeHash(testFile);
+
+				actual.Should().Be(expected);
+			}
+			finally
+			{
+				File.Delete(testFile);
+			}
+		}
+
+		[Test]
+		public void Should_Calculate_Expected_Checksum_From_Large_File_Path()
+		{
+			const string expected = "7FABFF96C4AE5491927AB5CE7EB9C6BAFD6E846E";
+			var testFile = CreateTestFile(int.MaxValue); // ~2 GB
+
+			try
+			{
+				var actual = provider.ComputeHash(testFile);
+
+				actual.Should().Be(expected);
+			}
+			finally
+			{
+				File.Delete(testFile);
+			}
+		}
+
+		[Test]
+		public void Should_Calculate_Expected_Checksum_From_Semi_Large_File_Path()
+		{
+			const string expected = "2279EF95D5A34B9A1DC5EBB9CA72E766FE09FDD8";
+			var testFile = CreateTestFile(int.MaxValue / 2); // ~1 GB
+
+			try
+			{
+				var actual = provider.ComputeHash(testFile);
+
+				actual.Should().Be(expected);
+			}
+			finally
+			{
+				File.Delete(testFile);
+			}
+		}
+
+		private static string CreateTestFile(int fileSize)
+		{
+			const int SEED = 50_000;
+
+			var filePath = Path.GetTempFileName();
+
+			Span<byte> testBytes = stackalloc byte[4096];
+			using var stream = new FileStream(filePath, FileMode.OpenOrCreate);
+			var i = 0;
+			var writtenBytes = 0L;
+
+			while (writtenBytes < fileSize)
+			{
+				var rnd = new Random(SEED + i);
+				rnd.NextBytes(testBytes);
+				stream.Write(testBytes);
+				i++;
+				writtenBytes += testBytes.Length;
+			}
+
+			stream.Flush();
+
+			return filePath;
+		}
+	}
+}
