06386810ef6ab84e54685f950d8dfa711cae4cc4
AdmiringWorm <kim.nordmo@gmail.com>
2022-11-29 15:18:55 +0100

    chore: fix line length

--- src/VideoConverter/Extensions/RegistrationExtensions.cs
+++ src/VideoConverter/Extensions/RegistrationExtensions.cs
@@ -6,25 +6,34 @@ namespace VideoConverter.Extensions
 
 	internal static class RegistrationExtensions
 	{
-		public static void RegisterScoped<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(this IRegistrator registrator)
+		public static void RegisterScoped<
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(
+			this IRegistrator registrator)
 			where TService : class
 		{
 			registrator.Register<TService>(Reuse.ScopedOrSingleton);
 		}
 
-		public static void RegisterSingleton<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(this IRegistrator registrator)
+		public static void RegisterSingleton<
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(
+			this IRegistrator registrator)
 			where TService : class
 		{
 			registrator.Register<TService>(Reuse.Singleton);
 		}
 
-		public static void RegisterSingleton<TService, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImplementation>(this IRegistrator registrator)
+		public static void RegisterSingleton<
+			TService,
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImplementation>(
+			this IRegistrator registrator)
 			where TImplementation : class, TService
 		{
 			registrator.Register<TService, TImplementation>(Reuse.Singleton);
 		}
 
-		public static void RegisterTransient<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(this IRegistrator registrator)
+		public static void RegisterTransient<
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(
+			this IRegistrator registrator)
 			where TService : class
 		{
 			registrator.Register<TService>(Reuse.Transient);
