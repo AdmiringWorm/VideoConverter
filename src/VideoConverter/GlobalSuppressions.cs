66afbdbbde8ee541888d36788ceb2fb7202f7f49
AdmiringWorm <kim.nordmo@gmail.com>
2022-12-03 03:19:59 +0100

    build: suppress trimming override warning

---
+++ src/VideoConverter/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Trimming", "IL2092:'DynamicallyAccessedMemberTypes' on the parameter of method don't match overridden parameter of method. All overridden members must have the same 'DynamicallyAccessedMembersAttribute' usage.", Justification = "<Pending>", Scope = "member", Target = "~M:VideoConverter.DependencyInjection.TypeRegistrar.Register(System.Type,System.Type)")]
